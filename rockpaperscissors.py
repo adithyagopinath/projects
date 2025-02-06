d8dd01774d22a4a82353239143c0c09457519818
Adithya Gopinath <adithyag@umd.edu>
2025-02-05 19:42:20 -0500

    reformat

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -7,7 +7,7 @@ import random
 def mode():
     while True:
         try:
-            m = int(input("Would you like to play til a fixed score [option 1] or keep going until you want to stop [option 2]? Respond with 1 or 2."))
+            m = int(input("Would you like to play til a fixed score [option 1] or keep going until you want to stop [option 2]? Respond with 1 or 2. "))
             if m in [1,2]:
                 break
             else: 
@@ -30,18 +30,18 @@ def game(sc):
     if sc > 0:
         while user < sc and pc < sc:
           while True:
-            u_rps = input("\nRock, paper, scissors, go! (Select r, p, or s.)").lower()
+            u_rps = input("\nRock, paper, scissors, go! (Select r, p, or s.) ").lower()
             if u_rps in ['r', 'p', 's']:
                 break
             else:
-                print("\nYou didn't select rock (r), paper (p), or scissors (s). Try again.")
+                print("\nYou didn't select rock (r), paper (p), or scissors (s). Try again. ")
           pc_rps = random.choice(['r', 'p', 's'])
           turns += 1
           if u_rps == 'r' and pc_rps == 's' or u_rps == 's' and pc_rps == 'p' or u_rps == 'p' and pc_rps == 'r':
               print(f"\nCongrats. You chose {u_rps} while your opponent elected to go with {pc_rps}. You win this round!")  
               user += 1
           elif pc_rps == 'r' and u_rps == 's' or pc_rps == 's' and u_rps == 'p' or pc_rps == 'p' and u_rps == 'r':
-              print(f"\nYikes. Your opponent took this round by selecting {pc_rps} over your decision to go {u_rps}. Better luck next round.")
+              print(f"\nYikes. Your opponent took this round by selecting {pc_rps} over your decision to go {u_rps}. Better luck next round. ")
               pc += 1
           else:
               print(f"\nYou and your opponent were thinking alike with both of you choosing {pc_rps}")
@@ -55,7 +55,7 @@ def game(sc):
         cont = True
         while cont:
             while True:
-                u_rps = input("\nRock, paper, scissors, go! (Select r, p, or s.)").lower()
+                u_rps = input("\nRock, paper, scissors, go! (Select r, p, or s.) ").lower()
                 if u_rps in ['r', 'p', 's']:
                     break
                 else:
